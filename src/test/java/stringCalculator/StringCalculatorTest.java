ca6ec1b1d0a4013a3a1d8770855cfc368911129c
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-05-03 17:52:52 +0900

    "java lotto" : 지정구분자 기능 구현("//;\n")

--- src/test/java/stringCalculator/StringCalculatorTest.java
+++ src/test/java/stringCalculator/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package stringCalculator;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,12 +8,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTest {
+    @DisplayName("input 받기")
     @ParameterizedTest
     @ValueSource(strings = {"1,2", "1,2,3", "1,2:3"})
     void inputTest(String input) {
         System.out.println(input);
     }
 
+    @DisplayName("일반구분자 인풋값 테스트")
     @Test
     void normalClassifierTest() {
         StringCalculator ca =  new StringCalculator();
@@ -21,5 +24,16 @@ public class StringCalculatorTest {
         String[] classifier= ca.normalClassifier();
         assertThat(input.split(classifier[0])).isEqualTo(expected);
     }
+
+    @Test
+    void specialClassifierTest() {
+        StringCalculator ca =  new StringCalculator();
+        String input = "\"//;\\n";
+        String[] expected = {"\"//","\\n"};
+        String[] result= ca.specialClassifier(input);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    
 
 }
